680a9e3edde16098705ada7292a0f7066363ce85
Kaiser <kaiser-benjamin@Hotmail.com>
2025-11-12 10:26:17 +0100

    Opdracht 1 Done!
    Hier is de eerste opdracht die wij moeten maken voor C#

---
+++ Medailles Opdrachten/Medailles Opdrachten/NaamEnLeeftijd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medailles_Opdrachten
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo, wat is jouw naam?");
+            String input = Console.ReadLine();
+            Console.WriteLine("Dat is perfect! Wat is jouw leeftijd?");
+            String leefTijd = Console.ReadLine();
+            Console.WriteLine("Leuk om je te ontmoeten, " + input + "!" + " " + "Als het goed is, ben jij " + leefTijd + "!");
+        }
+    }
+}
