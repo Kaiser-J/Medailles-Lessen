d646e6c7a10d061de70abb1454fe20e853a77f3f
Kaiser <kaiser-benjamin@Hotmail.com>
2026-01-15 13:31:22 +0100

    Added to Folder

---
+++ Medailles Opdrachten/Medailles Opdrachten/Opdracht 5/Tables.cs
@@ -0,0 +1,15 @@
+
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+
+
+
+for (int i = 1; i <= 10; i++)
+{
+    int result = 5 * i;
+    Console.WriteLine("5 x " + i + " = " + result);
+}
